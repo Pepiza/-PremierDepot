60f9174d9cdf2058e81000e4fa25181394a76183
Pepiza <101861250+Pepiza@users.noreply.github.com>
2022-03-18 08:59:13 +0100

    Oreration.cpp version 1.1
    added multiplication code and division code

--- Operations.cpp
+++ Operations.cpp
@@ -17,3 +17,23 @@ int soustraction(int a, int b)
 	
 	return resultat;
 }
+
+int Division(int a , int b)
+{
+	
+	int resultat;
+	
+	resultat = a/b;
+	
+	return resultat;
+}
+
+int Multiplication(int a , int b)
+{
+	
+	int resultat;
+	
+	resultat = a * b;
+	
+	return resultat;
+}
